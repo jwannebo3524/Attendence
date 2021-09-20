993efe2b05a4673c8e8cff10b6749890001d0de5
jwaters2461 <55212564+jwaters2461@users.noreply.github.com>
2021-09-20 18:14:12 -0500

    write.py now only accepts 6 digit numbers

--- write.py
+++ write.py
@@ -1,4 +1,5 @@
 barcode = input()
-barcodefile = open("barcodes.txt", "a")
-barcodefile.write(barcode + "\n")
-barcodefile.close()
+if int(barcode) > 99999 and int(barcode) < 1000000:
+    barcodefile = open("barcodes.txt", "a")
+    barcodefile.write(barcode + "\n")
+    barcodefile.close()

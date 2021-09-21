bb410d7b397ff052cd2745575360ac7824aaf78e
dfruzyna2137 <43018879+dfruzyna2137@users.noreply.github.com>
2021-09-20 19:38:29 -0500

    simplified

--- StudentID.py
+++ StudentID.py
@@ -1,16 +1,9 @@
 import random
 code = random.randint(0,999999)
-school = None
-year = None
-classNum = None
-temp = code
-codeLength = 0 
-
 if code > 999999:
     print("oh no!")
 else:
     classNum = code % 1000
     year = int(code/1000) % 100
-    school = int(code/100000) 
-    
+    school = int(code/100000)
     print (str(school) + "." + str(year) + "." + str(classNum))

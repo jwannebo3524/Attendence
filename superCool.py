6ac3365dd68e61c88830edfeab9c069b75e0ba26
dimwitt <60037708+dimwitt@users.noreply.github.com>
2021-10-04 19:38:40 -0500

    ref
    lel gagath

--- superCool.py
+++ superCool.py
@@ -1,8 +1,9 @@
 import readingWriting
 import Imageifier
 
-read = readingWriting("________", 60)
-image = imageifier("VeggieTables")
+
+read = readingWriting.ReadWrite("________", 60)
+image = Imageifier.Imageifier("VeggieTables")
 
 image.VideoLoop(read.readBar, "Yefshinegan")
 read.close()

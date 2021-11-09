a45ddd80e4f774462b3b6d734682847ecc9600e6
jwannebo3524 <56098116+jwannebo3524@users.noreply.github.com>
2021-11-08 18:39:22 -0600

    Update Imageifier.py
    exit condition. q keypressed or timeout

--- Imageifier.py
+++ Imageifier.py
@@ -1,6 +1,7 @@
 import time
 import picamera
 import numpy as np
+import keyboard
 
 class Imageifier:
   def __init__(self,file,startupDelay = 2,delay = 0.01,resolution = (1024,768),preveiw = False,framerate = 60):
@@ -29,6 +30,7 @@ class Imageifier:
       camera.start_recording(Object,format = 'rgb')
       while not Exit.Value:
         donothingvar = 0
+        Exit.Update()
       camera.stop_recording()
   def ImageLoop(self,Object,Exit):
     with picamera.Picamera as camera:
@@ -40,5 +42,13 @@ class Imageifier:
         camera.capture(out,'rgb')
         Object.run(out)
         time.sleep(self.Delay2)
-        
-      
+class Exit:
+  def __init__(self):
+    self.starttime = time.time()
+    self.Value = False
+    self.MAX = 60*5 #five minutes
+  def Update(self):
+    if ((time.time()-self.starttime)>self.MAX):
+      self.Value = True
+    elif (keyboard.is_pressed('q')):
+      self.Value = True

ca5c628728167cc91ca19551b4bc31c568cba6bb
jwannebo3524 <56098116+jwannebo3524@users.noreply.github.com>
2021-11-15 16:51:35 -0600

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,6 +1,7 @@
 import Imageifier as im
 import readingWriting as rw
-
+import datetime
+TodayPath = str(datetime.date.month)+str(datetime.date.day)+"-wildstang_attendance"
 i = im.Imigeifier('null')
 e = im.Exit()
 readwrite = rw.ReadWrite(TodayPath,60)
